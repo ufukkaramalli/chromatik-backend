4dfc518801e4c7b2f1a690891cefec5e8cedb5a6
Ufuk Karamalli <ufukkaramalli@gmail.com>
2022-09-13 03:45:02 +0300

    update, delete methods added and code clearing

--- src/controller/soundkit.controller.ts
+++ src/controller/soundkit.controller.ts
@@ -6,6 +6,8 @@ import validate from '@/utils/validation/soundkit.validation'
 import SoundkitService from '@/service/soundkit.service';
 import authenticated from '@/middleware/authenticated.middleware';
 import roleMiddleware from "@/middleware/role.middleware";
+import { Types } from "mongoose";
+import ISoundkit from "@/interfaces/soundkit.interface";
 
 class SoundkitController implements IController {
     public path = '/soundkit';
@@ -13,26 +15,58 @@ class SoundkitController implements IController {
     private SoundkitService = new SoundkitService();
 
     constructor(){
+        /** Route Guard */
+        this.router.use(`${this.path}`,[authenticated,roleMiddleware])
         this.initialiseRoutes();
-
-        /**
-         * Authentication Guard
-         */
-        
-        this.router.use(authenticated)
     }
 
     private initialiseRoutes(): void {
-
+        
         /**
-         * Create new Soundkit
+        * CREATE SOUNDKIT ROUTE // 
+        * Define Route Path ex. `${this.path}/:id`
+        * Define Middlewares ex. [authenticated, roleMiddleware, validationMiddleware(validate.create)]
+        * calls private update handler    
         */
 
-        this.router.post(`${this.path}`,[roleMiddleware,validationMiddleware(validate.create)],this.create)
+        this.router.post(
+
+            `${this.path}`,
+            [
+                validationMiddleware(validate.create)
+            ],
+            this.create
+        )
 
         /**
-         * Update soundkit
-         */
+        * UPDATE SOUNDKIT ROUTE // 
+        * Define Route Path ex. `${this.path}/:id`
+        * Define Middlewares ex. [authenticated, roleMiddleware, validationMiddleware(validate.create)]
+        * calls private update handler    
+        */
+
+         this.router.put(
+            `${this.path}/:id`,
+            [
+                validationMiddleware(validate.create)
+            ],
+            this.update
+        )
+
+        /**
+        * DELETE SOUNDKIT ROUTE // 
+        * Define Route Path ex. `${this.path}/:id`
+        * Define Middlewares ex. [authenticated, roleMiddleware, validationMiddleware(validate.create)]
+        * calls private update handler    
+        */
+
+         this.router.delete(
+            `${this.path}/:id`,
+            [
+                validationMiddleware(validate.deleteSoundkit)
+            ],
+            this.delete
+        )
     }
 
     private create = async (req:Request,res:Response,next: NextFunction): Promise<Response | void> => {
@@ -42,6 +76,34 @@ class SoundkitController implements IController {
             res.status(201).json({soundkit})
         } catch (error:any) {
             next(new HttpException(400, 'Cannot create soundkit'))
+        }
+    }
+
+    private update = async (req:Request, res:Response, next: NextFunction): Promise<Response | void> => {
+        try {
+            const { id } = req.params
+            const { title, description, thumbnailUrl, url, userId } = req.body
+            if(req.user.id !== userId){
+                next(new HttpException(400, 'You are not authorized to update this soundkit'))
+            }
+            const soundkit = await this.SoundkitService.update(id,title,description,thumbnailUrl,url, userId)
+            res.status(201).json({soundkit})   
+        } catch (e) {
+            next(new HttpException(400, 'Cannot update soundkit'))
+        }
+    }
+
+    private delete = async (req: Request, res: Response, next: NextFunction) : Promise<Response | void> => {
+        try {
+            const { id } = req.params
+            const { userId } = req.body
+            if(req.user.id !== userId){
+                next(new HttpException(400, 'You are not authorized to delete this soundkit'))
+            }
+            const soundkit = await this.SoundkitService.delete(id, userId)
+            res.status(201).json({soundkit})
+        } catch (error) {
+            next(new HttpException(400, 'Cannot delete soundkit'))
         }
     }
 }
--- src/service/soundkit.service.ts
+++ src/service/soundkit.service.ts
@@ -1,6 +1,9 @@
 import SoundkitModel from "@/model/soundkit.model";
 import ISoundkit from "@/interfaces/soundkit.interface";
 import { Types } from "mongoose";
+import { IUpdateOne } from "@/interfaces/mongoose.interface";
+
+
 
 class SoundkitService {
     private soundkit = SoundkitModel
@@ -14,7 +17,26 @@ class SoundkitService {
             const soundkit = await this.soundkit.create({title, description, thumbnailUrl, url, userId})
             return soundkit
         } catch (error) {
-            throw new Error('Unable to create soundkit')
+            throw new Error('Unable to create soundkit service')
+        }
+    }
+
+    public async update(id:string, title:string, description:string, thumbnailUrl:string, url:string, userId:Types.ObjectId): Promise<ISoundkit | IUpdateOne> {
+        try {
+            const updatedSoundkit = {title: title, _id: id, description: description, thumbnailUrl:thumbnailUrl, url:url, userId: userId}
+            const soundkit = await this.soundkit.updateOne({_id: id}, updatedSoundkit,{new: true})
+            return soundkit
+        } catch (error) {
+            throw new Error('Unable to update soundkit service')
+        }
+    }
+
+    public async delete(id:string, userId:Types.ObjectId): Promise<ISoundkit | null> {
+        try {
+            const soundkit = await this.soundkit.findOneAndDelete({_id:id,userId:userId})
+            return soundkit
+        } catch (error) {
+            throw new Error('Unable to delete soundkit service')
         }
     }
 }
